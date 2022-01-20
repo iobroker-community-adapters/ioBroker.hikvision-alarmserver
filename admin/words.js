aa998caabac72310472c9c06d9e55eadc70766b2
Robin Rainton <robin@rainton.com>
2022-01-20 11:06:27 +0100

    create-adapter stuff

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'hikvision-alarmserver adapter settings': {
+        'en': 'Adapter settings for hikvision-alarmserver',
+        'de': 'Adaptereinstellungen für hikvision-alarmserver',
+        'ru': 'Настройки адаптера для hikvision-alarmserver',
+        'pt': 'Configurações do adaptador para hikvision-alarmserver',
+        'nl': 'Adapterinstellingen voor hikvision-alarmserver',
+        'fr': "Paramètres d'adaptateur pour hikvision-alarmserver",
+        'it': "Impostazioni dell'adattatore per hikvision-alarmserver",
+        'es': 'Ajustes del adaptador para hikvision-alarmserver',
+        'pl': 'Ustawienia adaptera dla hikvision-alarmserver',
+        'zh-cn': 'hikvision-alarmserver的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
