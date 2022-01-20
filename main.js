997f4a40ae8a218bed821f7fbe009719a004b631
Robin Rainton <robin@rainton.com>
2022-01-20 12:38:44 +0100

    Seems to work

--- main.js
+++ main.js
@@ -8,8 +8,8 @@
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
 
-// Load your modules here, e.g.:
-// const fs = require("fs");
+const http = require('http');
+const parseString = require('xml2js').parseString;
 
 class HikvisionAlarmserver extends utils.Adapter {
 
@@ -22,32 +22,84 @@ class HikvisionAlarmserver extends utils.Adapter {
             name: 'hikvision-alarmserver',
         });
         this.on('ready', this.onReady.bind(this));
-        this.on('stateChange', this.onStateChange.bind(this));
-        // this.on('objectChange', this.onObjectChange.bind(this));
-        // this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
+
+        this.timers = [];
+        this.server = null;
     }
 
     /**
      * Is called when databases are connected and adapter received configuration.
      */
     async onReady() {
-        // Initialize your adapter here
+        const that = this;
+        this.server = http.createServer(function (request, response) {
+            if (request.method == 'POST') {
+                let body = '';
+                request.on('data', function (data) {
+                    body += data;
+                });
+                request.on('end', function () {
+                    that.log.debug(body);
+                    parseString(body, function (err, xml) {
+                        if (err) {
+                            that.log.error('Error parsing body: ' + err);
+                        } else {
+                            that.logEvent(xml.EventNotificationAlert);
+                        }
+                    });
 
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
+                });
+            }
+            response.end();
+        });
 
-        /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-        */
-        await this.setObjectNotExistsAsync('testVariable', {
+        this.server.listen(this.config.port);
+        this.log.info('Server listening on port ' + this.config.port);
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    async onUnload(callback) {
+        try {
+            if (this.server) {
+                this.server.close();
+                this.log.info('Closed server');
+            }
+            /* Clear any timers and set all to false immediately */
+            Object.keys(this.timers).forEach(async (id) => {
+                if (this.timers[id]) {
+                    this.clearTimeout(this.timers[id]);
+                    this.timers[id] = null;
+                }
+                await this.setStateAsync(id, false, true);
+            });
+
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    async logEvent(event) {
+        /* TODO: make object names configurable? Mac? IP? etc. */
+        const deviceId = event.macAddress;
+        const eventType = event.eventType;
+        const id = deviceId + '.' + eventType;
+
+        // Cancel any existing timer for this state
+        if (id in this.timers && this.timers[id]) {
+            this.clearTimeout(this.timers[id]);
+            this.timers[id] = null;
+        }
+
+        // Create state if not there
+        await this.setObjectNotExistsAsync(id, {
             type: 'state',
             common: {
-                name: 'testVariable',
+                name: eventType,
                 type: 'boolean',
                 role: 'indicator',
                 read: true,
@@ -56,103 +108,15 @@ class HikvisionAlarmserver extends utils.Adapter {
             native: {},
         });
 
-        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
-        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-        // this.subscribeStates('lights.*');
-        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-        // this.subscribeStates('*');
+        // Set it true (event in progress)
+        await this.setStateAsync(id, true, true);
 
-        /*
-            setState examples
-            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-        */
-        // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
-
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
-
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
-
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
+        // ... and restart to clear (set false) after 5s
+        this.timers[id] = this.setTimeout(() => {
+            this.setState(id, false, true);
+            this.timers[id] = null;
+        }, 5000 /* TODO: make timeout option? */);
     }
-
-    /**
-     * Is called when adapter shuts down - callback has to be called under any circumstances!
-     * @param {() => void} callback
-     */
-    onUnload(callback) {
-        try {
-            // Here you must clear all timeouts or intervals that may still be active
-            // clearTimeout(timeout1);
-            // clearTimeout(timeout2);
-            // ...
-            // clearInterval(interval1);
-
-            callback();
-        } catch (e) {
-            callback();
-        }
-    }
-
-    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-    // /**
-    //  * Is called if a subscribed object changes
-    //  * @param {string} id
-    //  * @param {ioBroker.Object | null | undefined} obj
-    //  */
-    // onObjectChange(id, obj) {
-    //     if (obj) {
-    //         // The object was changed
-    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-    //     } else {
-    //         // The object was deleted
-    //         this.log.info(`object ${id} deleted`);
-    //     }
-    // }
-
-    /**
-     * Is called if a subscribed state changes
-     * @param {string} id
-     * @param {ioBroker.State | null | undefined} state
-     */
-    onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted`);
-        }
-    }
-
-    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-    //  * @param {ioBroker.Message} obj
-    //  */
-    // onMessage(obj) {
-    //     if (typeof obj === 'object' && obj.message) {
-    //         if (obj.command === 'send') {
-    //             // e.g. send email or pushover or whatever
-    //             this.log.info('send command');
-
-    //             // Send response in callback if required
-    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-    //         }
-    //     }
-    // }
-
 }
 
 if (require.main !== module) {
